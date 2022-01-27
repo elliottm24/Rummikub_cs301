a9ba61a04eb117563d296b3e4c737dd6c2b9b52c
elliottm24 <elliottm24@up.edu>
2022-01-27 10:45:11 -0800

    Added place holder for clock and made the quit button smaller.

--- app/src/main/java/com/example/rummikub/MainActivity.java
+++ app/src/main/java/com/example/rummikub/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         class quitGame implements View.OnClickListener{
+            //Quit button
             Button quitButton = findViewByID(R.id.button);
 
             @Override
