f7403e7cc4bf4e0e31fd0e8aa8503c55071805c8
elliottm24 <elliottm24@up.edu>
2022-01-27 10:34:01 -0800

    Attempting to impliment quit button

--- app/src/main/java/com/example/rummikub/MainActivity.java
+++ app/src/main/java/com/example/rummikub/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.rummikub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        class quitGame implements View.OnClickListener{
+            Button quitButton = findViewByID(R.id.button);
+
+            @Override
+            public void onClick(View button) {
+                finish();
+                System.exit(0);
+            }
+        }
     }
 }
