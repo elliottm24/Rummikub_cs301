465a981ec9c460b6b75611b42641160e0de44fa3
elliottm24 <elliottm24@up.edu>
2022-01-29 14:49:42 -0800

    Started work on countdown timer. (changes made in MainActivity)

--- app/src/main/java/com/example/rummikub/MainActivity.java
+++ app/src/main/java/com/example/rummikub/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.rummikub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView timer = (TextView)findViewById(R.id.textView);
+
+        new CountDownTimer(3000,1000) {
+
+            @Override
+            public void onTick(long l) {
+                textTimer.setText("0:" + checkNum(time));
+                time--;
+            }
+
+            @Override
+            public void onFinish() {
+                textTimer.setText("Time's up");
+            }
+        }.start();
+
+        //public String checkNum(int num) {
+        //    return num <= 9 ? "0" + num : String.valueOf(num);
+        //}
 
         //Cite later: https://www.youtube.com/watch?v=Yx63M4k-EeE
         Button qButton = (Button) findViewById(R.id.button);
