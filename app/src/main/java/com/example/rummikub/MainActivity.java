81215d34a338269e2b8adc402896c092cdf02ad3
Maja (they/them) <94635445+elliottm24@users.noreply.github.com>
2022-02-24 12:00:47 -0800

    Last functioning version of the code. Use for part d.

--- app/src/main/java/com/example/rummikub/MainActivity.java
+++ app/src/main/java/com/example/rummikub/MainActivity.java
@@ -8,33 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+/*
+@authors Jacob Arnez, Maja Elliott, Dylan Kim, Chase Ohmestede
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //TextView timer = (TextView)findViewById(R.id.textView);
-
-
-        //new CountDownTimer(3000,1000) {
-
-        //    @Override
-        //    public void onTick(long millisUntilFinished) {
-        //        textTimer.setText("0:" + checkNum(time));
-        //        time--;
-        //    }
-
-        //    @Override
-        //    public void onFinish() {
-        //        textTimer.setText("Time's up");
-        //    }
-        //}.start();
-
-        //public String checkNum(int num) {
-        //    return num <= 9 ? "0" + num : String.valueOf(num);
-        //}
 
         //Cite later: https://www.youtube.com/watch?v=Yx63M4k-EeE
         Button qButton = (Button) findViewById(R.id.button);
