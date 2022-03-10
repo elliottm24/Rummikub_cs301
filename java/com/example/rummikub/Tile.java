5cec396eda5043116485283120e36624db412d3c
Chase Ohmstede <aceohmstede@gmail.com>
2022-03-10 10:52:36 -0800

    RummiView and Friends
    not within the original commit - adding the jank files!

---
+++ java/com/example/rummikub/Tile.java
@@ -0,0 +1,34 @@
+package com.example.rummikub;
+
+/**
+ * @authors Jacob Arnez, Maja Elliott, Dylan Kim, Chase Ohmstede
+ * */
+
+public class Tile {
+    public int color;
+    public int tileNum;
+
+    public Tile(int initColor, int initTileNum) {
+        this.color = initColor;
+        this.tileNum = initTileNum;
+    }
+
+    public Tile() {
+    }
+
+    /**
+     * For some reason Dylan made a deep copy ctor for Tile
+     * May delete later?
+     *
+     *     public Tile(Tile orig) {
+     *         this.color = orig.color;
+     *         this.tileNum = orig.tileNum;
+     *     }
+     *
+    **/
+
+    @Override
+    public String toString() {
+        return color + " " + tileNum;
+    }
+}
